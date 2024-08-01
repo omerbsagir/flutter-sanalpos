b57cb388fee79fcbf960855ddbc61a8647aabc03
omerbsagir <omerbesagir@gmail.com>
2024-08-01 10:05:37 +0300

    API Url'i eklendi

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,3 @@
 class Constants {
-  static const String apiBaseUrl = 'https://api.example.com';
+  static const String apiBaseUrl = 'https://anr6g85crj.execute-api.eu-central-1.amazonaws.com/Test';
 }
