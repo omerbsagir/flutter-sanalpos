ad87d57efa5cd618e21c185fd275302f37212cb0
omerbsagir <omerbesagir@gmail.com>
2024-08-08 10:12:22 +0300

    mainactivity düzeltildi

--- android/app/src/main/kotlin/com/example/flutterprojects/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutterprojects/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.example.flutterprojects
+/*package com.example.flutterprojects
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity()*/
