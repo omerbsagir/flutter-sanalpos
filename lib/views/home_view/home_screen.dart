0cd23ff58674123980b8716c92e5d5b228f119b0
omerbsagir <omerbesagir@gmail.com>
2024-08-10 21:38:49 +0300

    Düzeltme yapıldı

--- lib/views/home_view/home_screen.dart
+++ lib/views/home_view/home_screen.dart
@@ -8,7 +8,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  String _cardNumber = 'Tap your card to scan';
   TextEditingController _numberController = TextEditingController();
   bool _showNFCScan = false;
 
@@ -20,14 +19,70 @@ class _HomeScreenState extends State<HomeScreen> {
         setState(() {
           _cardNumber = ndefRecords.map((r) => r.payload).join(', ');
         });
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Center(
+              child: Text(
+                'NFC Tarama İşlemi Başarılı',
+                style: TextStyle(color: Colors.white), // Yazı rengi
+                textAlign: TextAlign.center, // Yazıyı ortalar
+              ),
+            ),
+            backgroundColor: Colors.green, // Arka plan rengi
+            behavior: SnackBarBehavior.floating, // Snackbar'ın ekranın biraz yukarısında görüntülenmesi için
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ), // Yuvarlak köşe
+            margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0), // Kenarlardan uzaklık
+          ),
+        );
+        _numberController.clear();
       } else {
         setState(() {
           _cardNumber = 'NDEF not available';
+          _numberController.clear();
+          _showNFCScan = false;
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Center(
+                child: Text(
+                  'NFC Tarama İşlemi Başarısız',
+                  style: TextStyle(color: Colors.white), // Yazı rengi
+                  textAlign: TextAlign.center, // Yazıyı ortalar
+                ),
+              ),
+              backgroundColor: Colors.red, // Arka plan rengi
+              behavior: SnackBarBehavior.floating, // Snackbar'ın ekranın biraz yukarısında görüntülenmesi için
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ), // Yuvarlak köşe
+              margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0), // Kenarlardan uzaklık
+            ),
+          );
         });
+
       }
     } catch (e) {
       setState(() {
-        _cardNumber = 'Error: $e';
+        _numberController.clear();
+        _showNFCScan = false;
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Center(
+              child: Text(
+                'NFC Tarama İşlemi Başarısız',
+                style: TextStyle(color: Colors.white), // Yazı rengi
+                textAlign: TextAlign.center, // Yazıyı ortalar
+              ),
+            ),
+            backgroundColor: Colors.red, // Arka plan rengi
+            behavior: SnackBarBehavior.floating, // Snackbar'ın ekranın biraz yukarısında görüntülenmesi için
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ), // Yuvarlak köşe
+            margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0), // Kenarlardan uzaklık
+          ),
+        );
       });
     } finally {
       await FlutterNfcKit.finish();
@@ -63,7 +118,21 @@ class _HomeScreenState extends State<HomeScreen> {
                       } else {
                         // Show an alert or a message if the input is empty
                         ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text('Please enter a number')),
+                          SnackBar(
+                            content: Center(
+                              child: Text(
+                                'Lütfen Geçerli Bir Sayı Girin !',
+                                style: TextStyle(color: Colors.white), // Yazı rengi
+                                textAlign: TextAlign.center, // Yazıyı ortalar
+                              ),
+                            ),
+                            backgroundColor: Colors.orange, // Arka plan rengi
+                            behavior: SnackBarBehavior.floating, // Snackbar'ın ekranın biraz yukarısında görüntülenmesi için
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ), // Yuvarlak köşe
+                            margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0), // Kenarlardan uzaklık
+                          ),
                         );
                       }
                     },
