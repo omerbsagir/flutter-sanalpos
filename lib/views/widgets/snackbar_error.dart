64725dd0d6d7165ba309daf8addb3007f24d6cbf
omerbsagir <omerbesagir@gmail.com>
2024-08-11 17:31:12 +0300

    Custom snackbar ayrı bir dart dosyasına taşındı

---
+++ lib/views/widgets/snackbar_error.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:vibration/vibration.dart';
+
+
+class CustomSnackbar {
+  static void show(BuildContext context,String message, Color backgroundColor ) {
+    final snackBar = SnackBar(
+      content: Text(
+        message,
+        textAlign: TextAlign.center,
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: backgroundColor,
+      behavior: SnackBarBehavior.floating,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    // Trigger a vibration when the SnackBar is shown.
+    final scaffold = ScaffoldMessenger.of(context);
+    scaffold.removeCurrentSnackBar();
+    scaffold.showSnackBar(snackBar);
+
+    if(backgroundColor == Colors.red) {
+      Vibration.vibrate(duration: 500);
+    }
+
+  }
+
+}
